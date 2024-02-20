0f149ea6c71dea47bd39ddebb3eb94d80abe2c70
phe9480 <99561631+phe9480@users.noreply.github.com>
2024-02-20 10:14:45 -0500

    Add files via upload

--- R/Bayesian.Hybrid.Design.R
+++ R/Bayesian.Hybrid.Design.R
@@ -16,9 +16,10 @@
 #' @param b0t Hyperprior for experimental response rate beta(a0t, b0t)
 #' @param delta_threshold Borrow when abs(pc_hat (current study) - pch) <= delta_threshold 
 #' @param method Method for dynamic borrowing, "Empirical Bayes" or "Heterogeneity".
+#' @param datamat A matrix with dimension nsim * 2 containing the pre-simulated data for the study treatment (1st column) and control  (1st column) groups, respectively.
 #' @param nsim Number of replications to calculate power
 #' @param seed=2000 seed for simulations
-#' @param method Method for dynamic borrowing, "Empirical Bayes", "Bayesian p", Density Product".
+#' @param method Method for dynamic borrowing, "Empirical Bayes", "Bayesian p", "Generalized BC", "JSD"
 #' 
 #' @return An object with values
 #'  \itemize{
@@ -36,14 +37,19 @@
 Bayesian.Hybrid.Design = function(pt=0.512, nt=40,pc=0.312,nc=40,pch=0.312,
                                   nche=40,nch=234, sig=0.90,
                                   a0c=0.001,b0c=0.001,a0t=0.001,b0t=0.001,
-                                  delta_threshold=0.1, method="Empirical Bayes",
+                                  delta_threshold=0.1, 
+                                  method="Empirical Bayes", theta=0.5,
+                                  datamat = NULL, w0 = NULL,
                                   nsim = 10000, seed=2000){
   
   #Dynamic borrowing parameter
-  wt = borrow.wt(Yc=nc*pc, nc=nc, Ych=nch*pch, nch=nch, nche=nche, 
-                 a0c=a0c, b0c=b0c, delta_threshold=delta_threshold, method=method)
-  w = wt$w
-  
+  if(is.null(w0)){
+    wt = borrow.wt(Yc=nc*pc, nc=nc, Ych=nch*pch, nch=nch, nche=nche, 
+                   a0c=a0c, b0c=b0c, delta_threshold=delta_threshold, method=method)
+    w = wt$w
+  } else {
+    w=w0
+  }
   #All scenarios for number of responders in exp arm
   Yt = 0:nt # start from 0
   Yc = nc*pc #number of responders in control arm
@@ -87,17 +93,33 @@ Bayesian.Hybrid.Design = function(pt=0.512, nt=40,pc=0.312,nc=40,pch=0.312,
   
   success = 0
   median_hca = median_c = rep(NA, nsim)
+  phat_pt_larger_pc_all = rep(NA, nsim)
+  
+  datamat2 = matrix(NA,nsim,2)
+  
+  wvec = numeric(nsim)
   
   #for each simulated trial, determine whether significant  
   for(i in 1:nsim){
-    Yt.s = rbinom(1,size=nt,prob=pt) #simulated Yt
-    Yc.s = rbinom(1,size=nc,prob=pc) #simulated Yc
+    if(is.null(datamat)){
+      Yt.s = rbinom(1,size=nt,prob=pt) #simulated Yt
+      Yc.s = rbinom(1,size=nc,prob=pc) #simulated Yc
+    } else{
+      Yt.s =datamat[i,1]
+      Yc.s =datamat[i,2]
+    }
+    
+    datamat2[i,] = c(Yt.s,Yc.s)
     
     #Dynamic borrowing parameter
-    wt = borrow.wt(Yc=Yc.s, nc=nc, Ych=nch*pch, nch=nch, nche=nche,  
-                   a0c=a0c, b0c=b0c, delta_threshold=delta_threshold, method=method)
-    w = wt$w
-    
+    if(is.null(w0)){
+      wt = borrow.wt(Yc=Yc.s, nc=nc, Ych=nch*pch, nch=nch, nche=nche,  
+                     a0c=a0c, b0c=b0c, delta_threshold=delta_threshold, 
+                     method=method, theta=theta)
+      w = wt$w
+    }else{
+      w = w0
+    }
     #Posterior distributions
     apost_c_trial = a0c + Yc.s
     bpost_c_trial = b0c + (nc-Yc.s)
@@ -120,10 +142,14 @@ Bayesian.Hybrid.Design = function(pt=0.512, nt=40,pc=0.312,nc=40,pch=0.312,
                                 at=apost_t,
                                 bt=bpost_t)$value
     
+    phat_pt_larger_pc_all[i] = phat_pt_larger_pc
+    
     success = success + (phat_pt_larger_pc>sig)
     
     median_hca[i] = qbeta(0.5, apost_c_hca, bpost_c_hca)
     median_c[i] = qbeta(0.5, apost_c_trial, bpost_c_trial)
+    
+    wvec[i] = w
     
   }
   power = success / nsim
@@ -133,6 +159,12 @@ Bayesian.Hybrid.Design = function(pt=0.512, nt=40,pc=0.312,nc=40,pch=0.312,
   return(list(power = power, 
               delta.bound=delta.bound, 
               pc.bias=pc.bias, 
-              pc.mse=pc.mse, nsim=nsim, seed=seed))
+              pc.mse=pc.mse, 
+              phat_pt_larger_pc_all = phat_pt_larger_pc_all,
+              median_hca = median_hca,
+              median_c = median_c,
+              simulated.data = datamat2,
+              nsim=nsim, seed=seed,
+              w=wvec))
 }
 
