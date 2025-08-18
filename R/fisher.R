b107fd57763dc9a1784013d9734b1ddd5233c38f
Zhaohua Lu <24821523+zhaohualu@users.noreply.github.com>
2025-08-17 23:06:28 -0400

    Update fisher.R

--- R/fisher.R
+++ R/fisher.R
@@ -1,31 +1,36 @@
-#' Fishers Exact Test for Response Rate in Two arms
+#' Fisher's Exact Test for a 2x2 Contingency Table
 #'
-#' This function performs Fishers exact test customized for comparing response rate for two arms.
+#' A wrapper for `stats::fisher.test` to conveniently compare response rates
+#' between two arms (experimental and control).
 #'
-#' @param Yc A scalar. Number of subjects with response in experimental arm
-#' @param Yt A scalar. Number of subjects with response  in control arm
-#' @param nt A scalar. Number of subjects in experimental arm
-#' @param nc A scalar. Number of subjects in control arm
+#' @param Yt A scalar integer. The number of subjects with a response in the
+#'   experimental (treatment) arm.
+#' @param nt A scalar integer. The total number of subjects in the
+#'   experimental arm.
+#' @param Yc A scalar integer. The number of subjects with a response in the
+#'   control arm.
+#' @param nc A scalar integer. The total number of subjects in the control arm.
+#' @param alternative A character string specifying the alternative hypothesis.
+#'   Must be one of "two.sided" (default), "greater" or "less".
 #'
-#' @return Fishers test object
+#' @return An object of class `htest` as returned by `stats::fisher.test`.
+#'
+#' @seealso \code{\link[stats]{fisher.test}}
+#'
 #' @examples
-#'
+#' \donttest{
 #' fisher(Yc=12, nc=40, Yt=19, nt=40)
+#' }
 #'
 #' @export
 #'
-fisher = function(Yc, nc, Yt, nt, alternative = "greater"){
-  #Yc: number of responders in control arm
-  #nc: number of subjects in control arm
-  #Yt: number of responders in experimental arm
-  #nt: number of subjects in experimental arm
-  #alternative: Default "greater"
-  #Example: fisher(Yc=12, nc=40, Yt=19, nt=40)
+fisher <- function(Yc, nc, Yt, nt, alternative = "greater") {
 
-  M <- as.table(rbind(c(Yt,Yc), c(nt-Yt, nc-Yc)))
+
+  M <- as.table(rbind(c(Yt, Yc), c(nt - Yt, nc - Yc)))
   dimnames(M) <- list(Response = c("Response", "Non-resp"),
-                      Group = c("Exp","control"))
+                      Group = c("Exp", "control"))
 
-  f = fisher.test(M, alternative = alternative)
+  f <- stats::fisher.test(M, alternative = alternative)
   return(f)
 }
